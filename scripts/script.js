122133bd2e9fcb2e9a919b99a6878a973aea3d18
ElaineBergstrom <elainebergstrom26@gmail.com>
2023-04-07 23:56:59 +0200

    fixed js and adjusted form grid/styling

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("register-form");
+const pass1 = document.getElementById("password");
+const pass2 = document.getElementById("confirm-password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInput();
+});
+
+const setError = (element, message) => {
+  const col = element.parentElement;
+  const errorDisplay = col.querySelector("#error");
+
+  errorDisplay.innerText = message;
+  col.classList.add("error");
+  col.classList.remove("success");
+};
+
+const setSuccess = (element) => {
+  const col = element.parentElement;
+  const errorDisplay = col.querySelector("#error");
+
+  errorDisplay.innerText = "";
+  col.classList.add("success");
+  col.classList.remove("error");
+};
+
+const validateInput = () => {
+  const passwordValue = pass1.value;
+  const passwordConfirmValue = pass2.value;
+
+  if (passwordConfirmValue !== passwordValue) {
+    setError(pass2, "* Passwords don't match!");
+    pass1.style.border = "2px solid red";
+    pass2.style.border = "2px solid red";
+  } else {
+    setSuccess(pass2);
+    pass1.style.border = "2px solid var(--gray-color)";
+    pass2.style.border = "2px solid var(--gray-color)";
+  }
+};
